b6ee032f27c6baabc2966a9b16a13136305c0bcf
Eugene Tereshchenko <i@jman.info>
2019-02-06 22:39:08 +0200

    first commit

---
+++ Magento/SampleForm/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\SampleForm\Controller\Adminhtml\Index;
+
+/**
+ * Responsible for loading page content.
+ *
+ * This is a basic controller that only loads the corresponding layout file. It may duplicate other such
+ * controllers, and thus it is considered tech debt. This code duplication will be resolved in future releases.
+ */
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+      parent::__construct($context);
+      $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Load the page defined in view/frontend/layout/samplenewpage_index_index.xml
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
