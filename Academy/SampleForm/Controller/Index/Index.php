24ce2534844f8c43423641e386792a346381903c
Eugene Tereshchenko <i@jman.info>
2019-02-06 23:11:12 +0200

    add result

---
+++ Academy/SampleForm/Controller/Index/Index.php
@@ -0,0 +1,40 @@
+<?php
+namespace Academy\SampleForm\Controller\Index;
+
+/**
+ * Responsible for loading page content.
+ *
+ * This is a basic controller that only loads the corresponding layout file. It may duplicate other such
+ * controllers, and thus it is considered tech debt. This code duplication will be resolved in future releases.
+ */
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+      parent::__construct($context);
+      $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Load the page defined in view/frontend/layout/samplenewpage_index_index.xml
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
